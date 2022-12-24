f382e628f5d191dafe9545399f62cea204b02508
Grossevich Lev <levgross@gmail.com>
2022-12-24 22:47:36 +0400

    redirect method from shortcut to url

--- src/main/java/ru/job4j/controller/ShortcutController.java
+++ src/main/java/ru/job4j/controller/ShortcutController.java
@@ -6,13 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.model.Shortcut;
 import ru.job4j.model.ShortcutDTOUrl;
 import ru.job4j.service.ShortcutService;
 import ru.job4j.service.SiteService;
+
+import java.net.URI;
 
 import static org.apache.commons.lang.RandomStringUtils.randomAlphanumeric;
 
@@ -47,5 +47,17 @@ public class ShortcutController {
                 response.toString(),
                 HttpStatus.OK
         );
+    }
+
+    @GetMapping("/redirect/{code}")
+    public ResponseEntity<Void> redirect(@PathVariable String code) {
+        var shortcutOpt = service.findByCode(code);
+        if (shortcutOpt.isEmpty()) {
+            return new ResponseEntity<>(
+                HttpStatus.NOT_FOUND
+            );
+        }
+        Shortcut shortcut = shortcutOpt.get();
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(shortcut.getUrl())).build();
     }
 }
