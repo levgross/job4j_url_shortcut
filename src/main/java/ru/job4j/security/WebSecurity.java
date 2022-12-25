706e846ae946e89228e076b45a1543cbeda67342
Grossevich Lev <levgross@gmail.com>
2022-12-25 23:42:33 +0400

    /redirect/** without authorization

--- src/main/java/ru/job4j/security/WebSecurity.java
+++ src/main/java/ru/job4j/security/WebSecurity.java
@@ -13,6 +13,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.context.annotation.Bean;
 
+import static ru.job4j.security.JWTAuthenticationFilter.REDIRECT_URL;
 import static ru.job4j.security.JWTAuthenticationFilter.SIGN_UP_URL;
 
 
@@ -26,6 +27,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.GET, REDIRECT_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
